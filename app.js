13eebfba657312052574b4b363180017816e278e
乐乐 <2729146193@qq.com>
2021-06-16 17:29:43 +0800

    node

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
 indexRouter(app)
 app.use(function (req, res, next) {
   let err = new Error('Not Found');
